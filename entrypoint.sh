47b0d01bc9d3d3c669b791519e7efe7d9f418239
Dviih <dviih@dviih.email>
2021-08-29 08:09:57 -0300

    forgot it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
 echo "Dviih GoTTY"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+${MODIFIED_STARTUP}
